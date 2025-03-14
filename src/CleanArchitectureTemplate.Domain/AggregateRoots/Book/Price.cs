11741cb9696130faa9342fe2d056c87d05dd2642
alirezaxcs2 <41263402+alirezaxcs2@users.noreply.github.com>
2025-03-14 20:42:32 +0100

    asd

--- src/CleanArchitectureTemplate.Domain/AggregateRoots/Book/Price.cs
+++ src/CleanArchitectureTemplate.Domain/AggregateRoots/Book/Price.cs
@@ -1,24 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchitectureTemplate.Shared
 {
     public record Price
     {
-        public decimal Value { get; set; }
+        public decimal Value { get; init; }
+
         public Price(decimal value)
         {
             if (value < 0)
-                throw new ArgumentException("invalid weight!");
+                throw new ArgumentOutOfRangeException(nameof(value), "Invalid price value!");
 
             Value = value;
-        
         }
-        public static implicit operator Price(decimal value) { return new Price(value); }
-        public static implicit operator decimal(Price value) { return value.Value; }
 
+        public static implicit operator Price(decimal value) => new Price(value);
+        public static implicit operator decimal(Price price) => price.Value;
     }
 }
--- src/CleanArchitectureTemplate.Infrastructure/AppDbContext.cs
+++ src/CleanArchitectureTemplate.Infrastructure/AppDbContext.cs
@@ -1,16 +1,37 @@
 using CleanArchitectureTemplate.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace  CleanArchitectureTemplate.Infrastructure;
+namespace CleanArchitectureTemplate.Infrastructure;
 public class AppDbContext : DbContext
 {
-    public DbSet<Book> Books { get; set; } // Example entity
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
-        optionsBuilder.UseSqlite("Data Source=./db/mydatabase.db");
     }
 
+    public DbSet<Book> Books { get; set; } // Example entity
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured) // Ensures this runs only if not configured by DI
+        {
+            optionsBuilder.UseSqlite("Data Source=./db/mydatabase.db");
+        }
+    }
 }
-
-
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.OwnsOne(b => b.Price, price =>
+        {
+            price.Property(p => p.Value).HasColumnName("Price");
+        });
+    }
+}
